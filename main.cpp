e155f75ff2bc343ad7723b8bf43142d87844a1ca
hardik-kansal <hkhardik4321@gmail.com>
2025-07-25 17:10:48 +0530

    getline

---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#include "enums.h"
+using namespace std;
+#define unsigned long long size_t
+#define signed long long ssize_t
+struct InputBuffer{
+    char* buffer;
+    size_t bufferLength;
+    ssize_t inputLength; // getline returns -1 in error , 0 if not read- rare
+};
+struct Statement{
+    StatementType type;
+};
+void print_prompt(){cout<<"db >";}
+InputBuffer* createEmptyBuffer(){
+    InputBuffer* inputBuffer=new InputBuffer();
+    inputBuffer->buffer=nullptr;
+    inputBuffer->bufferLength=0;
+    inputBuffer->inputLength=0;
+    return inputBuffer;
+}
+void read_input(InputBuffer* inputBuffer){
+    ssize_t bytesLength=getline(&(inputBuffer->buffer),&(inputBuffer->bufferLength),stdin);
+    if(bytesLength<=0)exit(EXIT_FAILURE);
+    inputBuffer->inputLength=bytesLength-1;
+    inputBuffer->buffer[bytesLength-1]=0;
+    // hello↵
+    // getline stores this hello\n\0
+    // \0 is null terminator
+    // \n \0 are single character and 0 and \0 means same in c c++ not '0';
+}
+
+
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer,Statement* statement) {
+  if (strcmp(input_buffer->buffer, "insert") == 0) {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  if (strcmp(input_buffer->buffer, "select") == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+  switch (statement->type) {
+    case (STATEMENT_INSERT):
+      printf("This is where we would do an insert.\n");
+      break;
+    case (STATEMENT_SELECT):
+      printf("This is where we would do a select.\n");
+      break;
+  }
+}
+
+
+int main(){
+    while (true){
+
+        InputBuffer* inputBuffer=createEmptyBuffer();
+        print_prompt();
+        read_input(inputBuffer);
+        if(inputBuffer->buffer[0]=='.'){
+            switch (do_meta_command(inputBuffer)){
+                case META_COMMAND_UNRECOGNIZED_COMMAND:
+                    cout<<"META_COMMAND_UNRECOGNIZED_COMMAND"<<endl;
+                    exit(EXIT_FAILURE);
+                case META_COMMAND_SUCCESS:
+                    continue;
+            }
+        }
+
+        Statement* statement=new Statement();
+        switch (prepare_statement(inputBuffer,statement)){
+            case PREPARE_UNRECOGNIZED_STATEMENT:
+                cout<<"PREPARE_UNRECOGNIZED_STATEMENT"<<endl;
+                exit(EXIT_FAILURE);
+            case PREPARE_SUCCESS:
+                continue;
+        }
+
+        execute_statement(statement);
+    }
+    return 0;
+}
